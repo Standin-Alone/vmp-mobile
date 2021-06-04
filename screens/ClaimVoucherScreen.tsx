bdf0d80e0b91ef2ca47b125e4a74f44288e40617
Standin-Alone <johnedcelzenarosa@gmail.com>
2021-06-04 15:20:19 +0800

    04-06-2021

--- screens/ClaimVoucherScreen.tsx
+++ screens/ClaimVoucherScreen.tsx
@@ -684,7 +684,7 @@ const showImage = (uri:any)=>{
                   rounded
                   type="numeric"
                   onChangeText={(value)=>{
-                                  setForm({...form,quantity_txt:value})
+                                  setForm({...form,quantity_txt:value.match})
                                   setForm((prevState)=>({...prevState,total_amount_txt:prevState.amount_txt * prevState.quantity_txt}))                
                                 }}
                   value={form.quantity_txt}
